a8b4289f89598b1a851c7b8de71bfb9aea89a6bb
harshikamboj <harshi.s.kamboj@gmail.com>
2025-03-21 00:47:07 -0400

    Updated to deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,40 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  )
-}
+      <footer className="footer">
+        <Container>
+          <Row className="align-items-center">
+            <Col size={12} sm={6}>
+              <img src={logo} alt="Logo" />
+            </Col>
+            <Col size={12} sm={6} className="text-center text-sm-end">
+              <div className="social-icon">
+                {/* Replace '#' with real links: */}
+                <a
+                    href="https://www.linkedin.com/in/harshikamboj/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  <img src={navIcon1} alt="LinkedIn" />
+                </a>
+                <a
+                    href="https://drive.google.com/file/d/1FbFUPZNxllNTbCWiNW_jeq60ouvR5EBa/view?usp=sharing"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  <img src={navIcon2} alt="Resume" />
+                </a>
+                <a
+                    href="https://github.com/harshikamboj"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  <img src={navIcon3} alt="GitHub" />
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+  );
+};
